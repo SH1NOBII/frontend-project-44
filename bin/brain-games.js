cb706eedb2cfa889a012cd37f8f0ceadf5167383
Shinobi <igordp7777777@gmail.com>
2023-09-04 20:49:34 +0300

    update

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
-import userName from '../src/cli.js'
+import userName from '../src/cli.js';
 
-
-// const  startGame = () => {
-    console.log('Welcome to the Brain Games!')
-    const name = userName();
-    console.log('Hi, ' + name + '!');
-// }
-
-// startGame();
+console.log('Welcome to the Brain Games!');
+const name = userName();
+console.log(`Hi, ${name}!`);
